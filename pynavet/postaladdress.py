384bec098a6e2feca678eae3a6e4109cd4748a57
Fredrik Thulin <fredrik@thulin.net>
2015-03-02 14:59:36 +0100

    Change from caching string and parsing every time to parse once.

--- pynavet/postaladdress.py
+++ pynavet/postaladdress.py
@@ -7,6 +7,8 @@ from suds import WebFault
 from xmltodict import parse as xmltodict
 from lxml import etree
 from logging import getLogger
+from collections import OrderedDict
+import pprint
 
 LOG = getLogger(__name__)
 
@@ -38,7 +40,7 @@ class PostalAddress(NavetClient):
         self.logger = getLogger()
         NavetClient.__init__(self, wsdl='wsdl/personpostXML.wsdl', cert=(cert, key_file), url=ws_url,
                              use_cache=use_cache, **kwargs)
-        # This plugin translates all XML-tags from Swedish to English
+        # This plugin translates all (known) XML-tags from Swedish to English
         self.load_plugin(MarshallXMLData)
 
     def get_all_data(self, identity_number, as_xml=False):
@@ -49,13 +51,14 @@ class PostalAddress(NavetClient):
         @type identity_number: str
         @param as_xml: If 'True' return the data as XML, if 'False' return data as an ordered dict (default: False)
         @type as_xml: bool
+        @return: Navet data, either as XML string or parsed (ordered) dict.
         """
         try:
             result = self.client.service.getData(self.order_id, identity_number)
             if not as_xml:
                 result = xmltodict(result)
             if self.debug:
-                self.logger.debug("Navet get_all_data lookup result:\n{!r}".format(result))
+                self.logger.debug("NAVET get_all_data lookup result:\n{!r}".format(result))
             return result
         except WebFault as e:
             LOG.error(e.message)  # TODO: Add translation for exceptions
@@ -71,18 +74,21 @@ class PostalAddress(NavetClient):
 
         @param identity_number: The national identity number to lookup
         @type identity_number: str
-        @param data: Results previously fetched with get_all_data(identity_number, as_xml=True) (optional)
+        @param data: Results previously fetched with get_all_data(identity_number, as_xml=False) (optional)
+        @type data: OrderedDict | None
         """
         try:
             if data is None:
-                data = self.get_all_data(identity_number, as_xml=True)
-            xml = etree.fromstring(data)
-            xml = xml.xpath('/NavetNotifications/PopulationItems/PopulationItem/PersonItem/PostalAddresses/OfficialAddress')
-            if len(xml) <= 0:
-                return False
-            result = xmltodict(etree.tostring(xml[0]))
+                data = self.get_all_data(identity_number, as_xml=False)
+            try:
+                person = data['NavetNotifications']['PopulationItems']['PopulationItem']['PersonItem']
+                result = OrderedDict([(u'OfficialAddress', person['PostalAddresses']['OfficialAddress']),
+                                      ])
+            except KeyError:
+                LOG.exception("NAVET address lookup failure")
+                result = False
             if self.debug:
-                self.logger.debug("Navet get_official_address result:\n{!r}".format(result))
+                self.logger.debug("NAVET get_official_address result:\n{!r}".format(result))
             return result
         except WebFault, e:
             raise e
@@ -94,18 +100,21 @@ class PostalAddress(NavetClient):
 
         @param identity_number: The national identity number to lookup
         @type identity_number: str
-        @param data: Results previously fetched with get_all_data(identity_number, as_xml=True) (optional)
+        @param data: Results previously fetched with get_all_data(identity_number, as_xml=False) (optional)
+        @type data: OrderedDict | None
         """
         try:
             if data is None:
-                data = self.get_all_data(identity_number, as_xml=True)
-            xml = etree.fromstring(data)
-            xml = xml.xpath('/NavetNotifications/PopulationItems/PopulationItem/PersonItem/Name')
-            if len(xml) <= 0:
-                return False
-            result = xmltodict(etree.tostring(xml[0]))
+                data = self.get_all_data(identity_number, as_xml=False)
+            try:
+                person = data['NavetNotifications']['PopulationItems']['PopulationItem']['PersonItem']
+                result = OrderedDict([(u'Name', person['Name']),
+                                      ])
+            except KeyError:
+                LOG.exception("NAVET name lookup failure")
+                result = False
             if self.debug:
-                self.logger.debug("Navet get_name result:\n{!r}".format(result))
+                self.logger.debug("NAVET get_name result:\n{!r}".format(result))
             return result
         except WebFault, e:
             raise e
@@ -117,22 +126,24 @@ class PostalAddress(NavetClient):
 
         @param identity_number: The national identity number to lookup
         @type identity_number: str
-        @param data: Results previously fetched with get_all_data(identity_number, as_xml=True) (optional)
+        @param data: Results previously fetched with get_all_data(identity_number, as_xml=False) (optional)
+        @type data: OrderedDict | None
         """
         try:
             if data is None:
-                data = self.get_all_data(identity_number)
+                data = self.get_all_data(identity_number, as_xml=False)
             if self.debug:
-                self.logger.debug("Naget get_name_and_official_address parsing:\n{!r}".format(data))
-            xml = etree.fromstring(data)
-            name = xml.xpath('/NavetNotifications/PopulationItems/PopulationItem/PersonItem/Name')
-            address = xml.xpath('/NavetNotifications/PopulationItems/PopulationItem/PersonItem/PostalAddresses/OfficialAddress')
-            if len(name) <= 0 or len(address) <= 0:
-                return False
-            result = xmltodict(etree.tostring(name[0]))
-            result.update(xmltodict(etree.tostring(address[0])))
+                self.logger.debug("NAVET get_name_and_official_address parsing:\n{!s}".format(pprint.pformat(data)))
+            try:
+                person = data['NavetNotifications']['PopulationItems']['PopulationItem']['PersonItem']
+                result = OrderedDict([(u'Name', person['Name']),
+                                      (u'OfficialAddress', person['PostalAddresses']['OfficialAddress']),
+                                      ])
+            except KeyError:
+                LOG.exception("NAVET name/address lookup failure")
+                result = False
             if self.debug:
-                self.logger.debug("Navet get_name_and_official_address result:\n{!r}".format(result))
+                self.logger.debug("NAVET get_name_and_official_address result:\n{!s}".format(pprint.pformat(result)))
             return result
         except WebFault, e:
             raise e
